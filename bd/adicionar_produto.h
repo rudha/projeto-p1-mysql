51546ba4f7321ef1c3fb891ba20e62f1019bf42e
Rudhá Oliveira <rudha94@gmail.com>
2014-05-23 12:33:46 -0300

    adicionar_produto.h

---
+++ bd/adicionar_produto.h
@@ -0,0 +1,58 @@
+struct produto
+{
+	int id;
+	char nome[101];
+	char fabricante[101];
+	float garantia;
+	char referencia[21];
+	int estado;
+};
+
+struct produto prod;
+
+int adicionar_produto (void)
+{		
+	int loop; // Para perguntar se o usuário quer adicionar mais produtos
+	
+	do
+	{
+		printf("Nome do produto a ser adicionado: ");
+		gets(prod.nome);
+		getchar;
+		printf("\n");
+		
+		printf("Fabricante do produto: ");
+		gets(prod.fabricante);
+		getchar();
+		printf("\n");
+		
+		printf("Garantia do produto(em anos): ");
+		scanf("%f", &prod.garantia);
+		getchar();
+		printf("\n");
+		
+		printf("Referencia do produto: ");
+		gets(prod.referencia);
+		getchar();
+		printf("\n");
+		
+		prod.estado=1;
+		
+		char query[2048];
+		sprintf(query, "INSERT INTO Produtos (Nome, Fabricante, Tempo_de_garantia, Referencia, Estado_do_item) VALUES (%s, %s, %f, %s, %d)", prod.nome, prod.fabricante, prod.garantia, prod.referencia, prod.estado);
+		
+		if (mysql_query(con, query))
+		{
+			finish_with_error(con);
+		}
+		
+		printf("\n");
+		printf("Produto adicionado com sucesso!\n");
+		printf("\n");
+		
+		printf("Quer adicionar outro item?(1-Sim, 2-Nao): ");
+		scanf("%d", &loop);
+		getchar();
+		printf("\n");
+	} while(loop==1);
+}
