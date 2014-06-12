99c40257167f43194bd3d45dcae09c98a42a98d7
bschron <bruno_16_6@hotmail.com>
2014-06-12 16:09:50 -0300

    Create adicionar_usuario.c

---
+++ bd/adicionar_usuario.c
@@ -0,0 +1,66 @@
+#include "headers.h"
+
+int adicionar_usuario (void)
+{
+	// String que acionará o "mysql_query":
+	char query[2048];
+	
+	// Limpar toda a tela:
+	system ("clear");
+	
+	// Escanear informações do usuário:
+	insert_login();
+	
+	insert_password();
+	
+	insert_name();
+	
+	insert_permission();
+	
+	sprintf(query, "INSERT INTO Contas_de_usuario (Login, Senha, Nome_completo, Nivel_permissoes) VALUES(%s, %s, %s, %d)", user_login, user_password, user_name, permission);
+	
+	// Consultar o BD:
+	if (mysql_query(con, query))
+	{
+		finish_with_error(con);
+		return 1;
+	}
+	return 0;
+}
+
+void insert_login (void)
+{
+	printf ("Informe o login do usuario:\n");
+	printf("(maximo de 20 caracteres)\n");
+	gets(user_login);
+	getchar();
+}
+
+void insert_password (void)
+{
+	printf("Informe a senha do usuario:\n");
+	printf("(maximo de 20 caracteres)\n");
+	gets(user_password);
+	getchar();
+}
+
+void insert_name (void)
+{
+	printf("Informe o nome completo do usuario:\n");
+	printf("(maximo de 100 caracteres)\n");
+	gets(user_name);
+	getchar();
+}
+
+void insert_permission (void)
+{
+	printf("Informe o nivel de permissoes do usuario:\n\n");
+   	printf("1 - Visualizar produtos e lotes apenas.\n");
+   	printf("2 - Visualizar lotes e produtos, e adicionar lotes.\n");
+   	printf("3 - Visualizar, editar e adicionar lotes e produtos.\n");
+   	printf("4 - Visualizar, editar e adicionar lotes, produtos e contas de usuario.\n");
+   	printf("\nINFORME APENAS O NUMERO DE PERMISSAO\n");
+   
+   	scanf("%d", &permission);
+   	getchar();
+}
