3b6e0fcebd572a688c802afcb43b703149795baa
bschron <bruno_16_6@hotmail.com>
2014-06-12 16:15:18 -0300

    Create headers.h

---
+++ bd/headers.h
@@ -0,0 +1,20 @@
+#ifndef HEADERS
+#define HEADERS
+
+#include <stdio.h>
+#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "adicionar_lote.h"
+#include "adicionar_produto.h"
+#include "adicionar_usuario.h"
+#include "editar_lote.h"
+#include "editar_produto.h
+#include "editar_usuario.h"
+#include "pesquisar_lote.h"
+#include "pesquisar_produto.h"
+#include "pesquisar_usuario.h"
+
+#endif
